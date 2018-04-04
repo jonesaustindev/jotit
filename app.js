9fa5096e7820aef28eb76eb781cde3701eb39342
Austin Jones <jonesaustindev@gmail.com>
2018-04-03 22:10:36 -0500

    changes to app.js

--- app.js
+++ app.js
@@ -88,10 +88,10 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, 'public')));
 
 // caching disabled for every route
-app.use(function(req, res, next) {
-  res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+//   next();
+// });
 
 
 
